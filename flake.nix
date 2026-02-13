d8c72685244b8246ea1979cbf321c9a0bc47b54f
Chris Dunphy <chris@megaparsec.ca>
2026-02-12 17:14:06 -0700

    Update Zellij config and flake
    Don't use flake-utils anymore

--- flake.nix
+++ flake.nix
@@ -2,34 +2,47 @@
   description = "This is a program that creates RSS feeds for any starred Github repos";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          # The packages we need for this project
-          buildInputs = [
-            pkgs.go_1_25
-            pkgs.go-tools
-            pkgs.gopls
-            pkgs.golangci-lint
-            pkgs.go-task
-            pkgs.bash-language-server
-            pkgs.docker-language-server
-            pkgs.yaml-language-server
-          ];
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      buildPkgsConf =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
         };
-      }
-    );
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (
+        system:
+        let
+          pkgs = buildPkgsConf system;
+        in
+        {
+          default = pkgs.mkShell {
+            # The packages we need for this project
+            buildInputs = [
+              pkgs.go_1_25
+              pkgs.go-tools
+              pkgs.gopls
+              pkgs.golangci-lint
+              pkgs.go-task
+              pkgs.bash-language-server
+              pkgs.docker-language-server
+              pkgs.yaml-language-server
+            ];
+          };
+        }
+      );
+    };
+
 }
