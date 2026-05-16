31c68e621755b4de98b77bc5c0fdd3eb0d803602
Chris Dunphy <chris@megaparsec.ca>
2026-05-15 20:27:15 -0600

    Refactor and clean-up

--- github/github.go
+++ github/github.go
@@ -3,9 +3,12 @@ package github
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 	"regexp"
+	"strings"
 
 	"github.com/atomicmeganerd/starfeed/config"
 )
@@ -41,7 +44,7 @@ func NewGitHubStarredFeedBuilder(
 }
 
 // This will return all starred repos including the Atom feeds for their releases
-// It returns a map of relaseFeedUrl -> GitHubRepo
+// It returns a map of releaseFeedUrl -> GitHubRepo
 func (gh *gitHubStarredFeedBuilder) GetStarredRepos(
 	ctx context.Context,
 ) (map[string]GitHubRepo, error) {
@@ -50,9 +53,7 @@ func (gh *gitHubStarredFeedBuilder) GetStarredRepos(
 	slog.Debug("Querying GitHub for starred repos", "url", getUrl)
 
 	for {
-		ghResponse, err := doApiRequest(
-			ctx, gh.client, getUrl, gh.cfg.GitHubToken, gh.nextPageLinkRegex,
-		)
+		ghResponse, err := gh.doApiRequest(ctx, getUrl)
 		if err != nil {
 			return nil, err
 		}
@@ -82,4 +83,59 @@ func (gh *gitHubStarredFeedBuilder) GetStarredRepos(
 // - feedUrl: The URL of the RSS feed to check.
 func (gh *gitHubStarredFeedBuilder) IsGitHubReleasesFeed(feedUrl string) bool {
 	return gh.isRelRepoRegex.MatchString(feedUrl)
+}
+
+func (gh *gitHubStarredFeedBuilder) doApiRequest(
+	ctx context.Context,
+	ghReqUrl string,
+) (*GitHubResponse, error) {
+	headers := map[string]string{
+		"Authorization":        fmt.Sprintf("Bearer %s", gh.cfg.GitHubToken),
+		"X-GitHub-Api-Version": "2022-11-28",
+		"User-Agent":           "github.com/atomicmeganerd/starfeed",
+		"Content-Type":         "application/json",
+		"Accept":               "application/json",
+	}
+
+	// No request will always be valid here so we can ignore the error
+	req, err := http.NewRequestWithContext(ctx, "GET", ghReqUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	res, err := gh.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close() // nolint: errcheck
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("github returned an http error code %d", res.StatusCode)
+	}
+
+	return gh.processGitHubResponse(res)
+}
+
+func (gh *gitHubStarredFeedBuilder) processGitHubResponse(
+	r *http.Response,
+) (*GitHubResponse, error) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	linkRaw := r.Header.Get("Link")
+	links := strings.SplitSeq(linkRaw, ",")
+	for link := range links {
+		matches := gh.nextPageLinkRegex.FindStringSubmatch(link)
+		if len(matches) == 2 {
+			return &GitHubResponse{data: data, nextPage: matches[1]}, nil
+		}
+	}
+
+	return &GitHubResponse{data: data}, nil
 }
