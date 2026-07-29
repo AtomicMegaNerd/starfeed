3129a75861d2fb22842872e0c61e0cb954f5965d
Chris Dunphy <chris@megaparsec.ca>
2026-07-28 22:36:38 -0600

    Re-add pre-commit to flake

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "This is a program that creates RSS feeds for any starred GitHub repos";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
-    { self, nixpkgs }:
+    { nixpkgs, git-hooks, ... }:
     let
       systems = [
         "x86_64-linux"
@@ -14,6 +15,35 @@
       ];
     in
     {
+      checks = nixpkgs.lib.genAttrs systems (system: {
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt.enable = true;
+            format = {
+              enable = true;
+              name = "Format Go code";
+              entry = "task format";
+              language = "system";
+              pass_filenames = false;
+            };
+            check-deps = {
+              enable = true;
+              name = "Check Go dependencies";
+              entry = "task check-deps";
+              language = "system";
+              pass_filenames = false;
+            };
+            lint = {
+              enable = true;
+              name = "Lint Go code";
+              entry = "task lint";
+              language = "system";
+              pass_filenames = false;
+            };
+          };
+        };
+      });
       devShells = nixpkgs.lib.genAttrs systems (
         system:
         let
